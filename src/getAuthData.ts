9a7aaa1c75ce72d158e839b464ea809f62be8ac2
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-06-20 11:48:36 +0200

    feat store cert and macaroon

--- src/getAuthData.ts
+++ src/getAuthData.ts
@@ -1,10 +1,31 @@
+import { readFile, writeFile } from "node:fs/promises";
 import { createInterface } from "node:readline/promises";
+import type { Interface } from "node:readline/promises";
+
+const getContents = async (name: string, readLineInterface: Interface) => {
+    try {
+        return await readFile(name, { encoding: "utf8" });
+    } catch (error: unknown) {
+        if (!(error instanceof Error)) {
+            throw error;
+        }
+
+        const contents = await readLineInterface.question(`${name}: `);
+        await writeFile(name, contents, { encoding: "utf8" });
+        return contents;
+    }
+};
 
 export const getAuthData = async () => {
     const { stdin: input, stdout: output } = process;
     const readlineInterface = createInterface({ input, output });
-    const cert = await readlineInterface.question("cert: ");
-    const macaroon = await readlineInterface.question("macaroon: ");
-    readlineInterface.close();
-    return { cert, macaroon };
+
+    try {
+        return {
+            cert: await getContents("tls.cert", readlineInterface),
+            macaroon: await getContents("admin.macaroon", readlineInterface),
+        };
+    } finally {
+        readlineInterface.close();
+    }
 };
