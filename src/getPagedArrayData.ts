c66d5e71f728247d0485545e3886455db25381e0
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-06-27 11:20:28 +0200

    refactor: extract getArgs

--- src/getPagedArrayData.ts
+++ src/getPagedArrayData.ts
@@ -2,6 +2,29 @@ import type { AuthenticatedLightningArgs, AuthenticatedLightningMethod, Paginati
 
 const toIsoString = (date: number) => new Date(date).toISOString();
 
+const getArgs = <const After extends string, const Before extends string>(
+    args: AuthenticatedLightningArgs<PaginationArgs> & OptionalArgs, after: After, before: Before, token?: string,
+) => {
+    // We need to remove token from args
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { days, limit, token: unused, ...pureArgs } = args;
+
+    if (token) {
+        return { ...pureArgs, token };
+    } else if (days) {
+        const span = days * 24 * 60 * 60 * 1000;
+
+        return {
+            ...pureArgs,
+            ...(limit ? { limit } : {}),
+            [after]: toIsoString(Date.now() - span),
+            [before]: toIsoString(Date.now()),
+        };
+    }
+
+    return { ...pureArgs, ...(limit ? { limit } : {}) };
+};
+
 export interface OptionalArgs {
     days?: number;
     limit?: number;
@@ -19,34 +42,15 @@ export const getPagedArrayData = async <
     before: Before,
     prop: Prop,
 ) => {
-    // We need to remove token from args
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { days, limit, token, ...pureArgs } = args;
-    let currentArgs: AuthenticatedLightningArgs<PaginationArgs>;
-
-    if (days) {
-        const span = days * 24 * 60 * 60 * 1000;
-
-        currentArgs = {
-            ...pureArgs,
-            ...(limit ? { limit } : {}),
-            [after]: toIsoString(Date.now() - span),
-            [before]: toIsoString(Date.now()),
-        };
-    } else {
-        currentArgs = { ...pureArgs };
-    }
-
+    let page: Array<Return[Prop][number]>;
+    let token: string | undefined;
     const result = new Array<Return[Prop][number]>();
-    let getNextPage: boolean;
 
     do {
         // eslint-disable-next-line no-await-in-loop
-        const { [prop]: page, next } = await func(currentArgs);
+        ({ [prop]: page, next: token } = await func(getArgs(args, after, before, token)));
         result.push(...page);
-        currentArgs = { ...pureArgs, ...(next ? { token: next } : {}) };
-        getNextPage = Boolean(days) && page.length > 0;
-    } while (getNextPage);
+    } while (Boolean(args.days) && page.length > 0);
 
     return result;
 };
