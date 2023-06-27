88497ac2c9d7a2b45ed6aaa1df0d2a21c80b00a5
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-06-27 10:24:20 +0200

    refactor: eliminate superfluous generic arguments

--- src/test/testPagedArrayResultFunction.ts
+++ src/test/testPagedArrayResultFunction.ts
@@ -1,13 +1,12 @@
 import { expect } from "chai";
-import type { AuthenticatedLightningArgs } from "lightning";
+import type { AuthenticatedLightningArgs, PaginationArgs } from "lightning";
 import type { OptionalArgs } from "../getPagedArrayData.js";
 import { connectLnd } from "./connectLnd.js";
 
-export const testPagedArrayResultFunction = <
-    Args extends AuthenticatedLightningArgs,
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Return extends Array<{ created_at: string }>,
->(func: (args: Args & OptionalArgs) => Promise<Return>) => {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const testPagedArrayResultFunction = <Return extends Array<{ created_at: string }>>(
+    func: (args: AuthenticatedLightningArgs<PaginationArgs> & OptionalArgs) => Promise<Return>,
+) => {
     describe(func.name, () => {
         it("should return results as requested", async function test2() {
             // eslint-disable-next-line @typescript-eslint/no-invalid-this
@@ -15,7 +14,7 @@ export const testPagedArrayResultFunction = <
 
             const resultCount = 3;
             const authenticatedLnd = await connectLnd(undefined, resultCount);
-            const { length } = await func(authenticatedLnd as Args & OptionalArgs);
+            const { length } = await func(authenticatedLnd);
             expect(length).to.be.below(resultCount + 1);
             expect(length).to.be.above(0);
         });
@@ -26,7 +25,7 @@ export const testPagedArrayResultFunction = <
 
             const now = Date.now();
             const authenticatedLnd = await connectLnd(1);
-            const results = await func(authenticatedLnd as Args & OptionalArgs);
+            const results = await func(authenticatedLnd);
 
             for (const result of results) {
                 expect(now - new Date(result.created_at).valueOf()).to.be.below(24 * 60 * 60 * 1000);
