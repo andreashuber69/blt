05ee83e67aaf2283aa12270842813ffced73b7ff
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-07-08 21:53:35 +0200

    feat: getNodeInfo

---
+++ src/getNodeInfo.ts
@@ -0,0 +1,68 @@
+import type { AuthenticatedLightningArgs, GetIdentityResult } from "lightning";
+import { getIdentity } from "lightning";
+import { getChannels } from "./getChannels.js";
+import { getForwards } from "./getForwards.js";
+import { getPayments } from "./getPayments.js";
+import type { YieldType } from "./YieldType.js";
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const toSortedArray = async <T extends { readonly created_at: string }>(generator: AsyncGenerator<T>) => {
+    const result = new Array<T>();
+
+    for await (const element of generator) {
+        result.push(element);
+    }
+
+    result.sort((a, b) => new Date(a.created_at).valueOf() - new Date(b.created_at).valueOf());
+    return result;
+};
+
+
+export type Identity = Readonly<GetIdentityResult>;
+
+export type Channel = Readonly<Awaited<ReturnType<typeof getChannels>>[number]>;
+
+export type Forward = Readonly<YieldType<ReturnType<typeof getForwards>>>;
+
+export type Payment = Readonly<YieldType<ReturnType<typeof getPayments>>>;
+
+/**
+ * Provides various information about a node.
+ * @description All time-bound data (like {@link NodeInfo.forwards}) will be sorted earliest to latest.
+ */
+export interface NodeInfo {
+    readonly identity: Identity;
+
+    /** The currently open channels. */
+    readonly channels: readonly Channel[];
+
+    /** The forwards routed through the node. */
+    readonly forwards: readonly Forward[];
+
+    /** The payments made from the node. */
+    readonly payments: readonly Payment[];
+}
+
+export interface NodeInfoArgs {
+    /** Retrieve time-bound data up to this number of days in the past. */
+    readonly days?: number;
+}
+
+/**
+ * Gets information about the Node.
+ * @param args The authenticated LND API object, optionally combined with a number how far back historical data should
+ * be retrieved. The default is 14 days.
+ */
+export const getNodeInfo = async (args: AuthenticatedLightningArgs<NodeInfoArgs>): Promise<NodeInfo> => {
+    const { days, ...lnd } = { days: 14, ...args };
+    const before = new Date(Date.now()).toISOString();
+    const after = new Date(new Date(before).valueOf() - (days * 24 * 60 * 60 * 1000)).toISOString();
+
+    return {
+        identity: await getIdentity(lnd),
+        channels: await getChannels(lnd),
+        forwards: await toSortedArray(getForwards({ after, before, ...lnd })),
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        payments: await toSortedArray(getPayments({ created_after: after, created_before: before, ...lnd })),
+    };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
 import { createRequire } from "node:module";
-import { deletePayment } from "lightning";
-import { getFailedPayments } from "./getFailedPayments.js";
+import { getNodeInfo } from "./getNodeInfo.js";
 import { connectLnd } from "./test/connectLnd.js";
 
 interface PackageJson {
@@ -17,28 +16,10 @@ try {
     // https://stackoverflow.com/questions/58172911/typescript-compiler-options-trying-to-get-flat-output-to-outdir
     const { name, version } = createRequire(import.meta.url)("../package.json") as PackageJson;
     console.log(`${name} v${version}`);
-
-    const oneDay = 24 * 60 * 60 * 1000;
     const start = Date.now();
-    const after = new Date(start - (365 * oneDay)).toISOString();
-    const before = new Date(start - (30 * oneDay)).toISOString();
-
-    const authenticatedLnd = await connectLnd();
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const failedPayments = getFailedPayments({ created_after: after, created_before: before, ...authenticatedLnd });
-    let deleteCount = 0;
-
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    for await (const { id, created_at } of failedPayments) {
-        console.log(created_at);
-        await deletePayment({ ...authenticatedLnd, id });
-
-        if (++deleteCount >= 1000) {
-            break;
-        }
-    }
-
-    console.log(`${(Date.now() - start) / 1000} ${deleteCount}`);
+    const nodeInfo = await getNodeInfo(await connectLnd());
+    console.log(`${(Date.now() - start) / 1000}`);
+    console.log(nodeInfo);
 } catch (error: unknown) {
     console.error(error);
     process.exitCode = 1;
