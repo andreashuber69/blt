aea939234bde4f9a5731405f591387002e07fd30
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-07-16 15:52:55 +0200

    refactor: rename & docs

---
+++ src/ScheduledCall.spec.ts
@@ -0,0 +1,39 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import { ScheduledCall } from "./ScheduledCall.js";
+
+const wait = async (delayMilliseconds: number) =>
+    await new Promise((resolve) => setTimeout(resolve, delayMilliseconds));
+
+describe(ScheduledCall.name, () => {
+    describe(ScheduledCall.prototype.call.name, () => {
+        const delay = 1000;
+
+        it("should delay execution", async () => {
+            const executor = new ScheduledCall(delay);
+            let done = false;
+            const task = () => void (done = true);
+            executor.call(task);
+            assert(!done);
+            await wait(delay);
+            assert(done);
+        });
+
+        it("should not make other calls while busy", async () => {
+            const executor = new ScheduledCall(delay);
+            let count = 0;
+            const task = () => void (++count);
+            executor.call(task);
+            await wait(delay / 10);
+            executor.call(task);
+            await wait(delay / 10);
+            executor.call(task);
+            assert(count === 0);
+            await wait(delay * 3);
+            assert(count as number === 1);
+            executor.call(task);
+            await wait(delay);
+            assert(count as number === 2);
+        });
+    });
+});
