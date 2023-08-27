5b94fc4de41168586c661708bd61c822ce0537b7
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-08-27 16:36:29 +0200

    feat: refresh channels after forward or payment

--- src/info/ChannelsRefresher.ts
+++ src/info/ChannelsRefresher.ts
@@ -1,8 +1,11 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
 import type {
-    AuthenticatedLightningArgs, SubscribeToChannelsChannelClosedEvent, SubscribeToChannelsChannelOpenedEvent,
+    AuthenticatedLightningArgs,
+    SubscribeToChannelsChannelClosedEvent,
+    SubscribeToChannelsChannelOpenedEvent,
+    SubscribeToForwardsForwardEvent,
 } from "lightning";
-import { subscribeToChannels } from "lightning";
+import { subscribeToChannels, subscribeToForwards, subscribeToPayments } from "lightning";
 
 import type { Channel } from "../lightning/getChannels.js";
 import { getChannels } from "../lightning/getChannels.js";
@@ -10,7 +13,7 @@ import { log } from "../Logger.js";
 import { FullRefresher } from "./FullRefresher.js";
 import type { Emitters, IRefresher } from "./Refresher.js";
 
-type ChannelsEmitters = Emitters<"channels">;
+type ChannelsEmitters = Emitters<"channels" | "forwards" | "payments">;
 
 export interface IChannelsRefresherArgs {
     /** The {@linkcode AuthenticatedLightningArgs} of the node the data should be retrieved from. */
@@ -37,7 +40,7 @@ export class ChannelsRefresher extends FullRefresher<"channels", Channel, Channe
         return await getChannels({ ...lndArgs, is_public: true });
     }
 
-    protected override onServerChanged({ channels }: ChannelsEmitters, listener: () => void) {
+    protected override onServerChanged({ channels, forwards, payments }: ChannelsEmitters, listener: () => void) {
         const handler = (e: SubscribeToChannelsChannelClosedEvent | SubscribeToChannelsChannelOpenedEvent) => {
             log(`channel ${e.id}`);
             listener();
@@ -45,10 +48,24 @@ export class ChannelsRefresher extends FullRefresher<"channels", Channel, Channe
 
         channels.on("channel_opened", handler);
         channels.on("channel_closed", handler);
+
+        // The Channel type also exposes the local and remote balance, which is why we need to refresh when a forward or
+        // a payment has been made.
+        forwards.on("forward", (e: SubscribeToForwardsForwardEvent) => {
+            if (e.is_confirmed) {
+                listener();
+            }
+        });
+
+        payments.on("confirmed", listener);
     }
 
     protected override createServerEmitters(lndArgs: AuthenticatedLightningArgs) {
-        return { channels: subscribeToChannels(lndArgs) } as const;
+        return {
+            channels: subscribeToChannels(lndArgs),
+            forwards: subscribeToForwards(lndArgs),
+            payments: subscribeToPayments(lndArgs),
+        } as const;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
