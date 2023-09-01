15ccb48d9f2bcef3a9ba3e1d458e55aa4e873603
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-01 14:40:14 +0200

    docs: outline how the Actions class works

--- src/Actions..ts
+++ src/Actions..ts
@@ -10,8 +10,8 @@ export interface ActionsConfig {
     readonly minChannelBalanceFraction: number;
 
     /**
-     * The maximum deviation a variable can have as a fraction of its maximum value before corrective actions are
-     * suggested.
+     * The maximum deviation from the target a variable can have as a fraction of its maximum value before corrective\
+     * actions are suggested.
      */
     readonly maxDeviationFraction: number;
 
@@ -32,6 +32,34 @@ export interface Action {
     readonly reason: string;
 }
 
+/**
+ * Suggests actions for a routing lightning node to get closer to profitability and avoid situations of low liquidity.
+ * @description The actions are calculated as outlined below:
+ * <ul>
+ * <li>Observe incoming and outgoing flow of each channel and set the local balance target to optimize liquidity.
+ * For example, very few channels have a good balance between incoming and outgoing forwards. It's much more likely for
+ * a channel to have &gt;95% of its routing flow going out or coming in. For these channels it makes little sense to set
+ * the balance target to half their capacity. On the other hand, it neither makes sense to target a local balance of
+ * &gt;95% or &lt;5% (depending on flow direction), as doing so would preclude most routing in the other direction. Such
+ * bidirectional routing is highly desirable (because it reduces rebalancing) and should therefore not be made
+ * impossible by low liquidity. This is why the suggested actions will not let channel balance go below or above a given
+ * limit (e.g. 25% and 75%).</li>
+ * <li>Set the target of the total local balance of the node to the sum of the target balances of all channels.</li>
+ * <li>Monitor individual channel balance. If the balance falls below certain thresholds, this means that the fee on the
+ * channel itself is too low and should therefore be raised immediately. If channel balance raises above certain
+ * thresholds, this means that the recent incoming flow was forwarded to channels with fees too low and the fees on
+ * those channels should be raised immediately.</li>
+ * <li>If the local balance stays close to or above the target and no forwarding flow is outgoing over long periods of
+ * time, this means that the fee on the channel is too high and should be reduced slowly until it either reaches 0 or
+ * outgoing flow reduces the balance.</li>
+ * <li>If the local balance stays substantially below the target and rebalancing success is very low, the fee is likely
+ * too low (as rebalancing sets the current fee as an upper limit) and should be increased slowly.</li>
+ * </ul>
+ * Note that the actions suggested by this class deliberately do not define how the targets should be reached. Some
+ * targets can be trivially reached with automation (e.g. fees) others are much harder (e.g. individual channel
+ * balances) or even even downright impossible to reach automatically (e.g. total node balance). An external component
+ * should therefore define how these actions should be implemented.
+ */
 export class Actions {
     public static get({ channels }: NodeStats, config: ActionsConfig) {
         const actions =
@@ -156,4 +184,6 @@ export class Actions {
 
         return createAction(optimalBalance, "This is the optimal balance according to flow.");
     }
+
+    private constructor() { /* Intentionally empty */ }
 }
