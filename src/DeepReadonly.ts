ed902010529e31c2c36b0de419115aa5818261ac
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-05 21:25:56 +0200

    fix: handle functions and arrays

--- src/DeepReadonly.ts
+++ src/DeepReadonly.ts
@@ -1,2 +1,8 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
-export type DeepReadonly<T> = T extends object ? { readonly [P in keyof T]: DeepReadonly<T[P]> } : T;
+export type DeepReadonly<T> =
+    /* eslint-disable @typescript-eslint/indent */
+    T extends Array<infer R> ? ReadonlyArray<DeepReadonly<R>> :
+    T extends () => unknown ? T :
+    T extends object ? { readonly [P in keyof T]: DeepReadonly<T[P]> } :
+    T;
+    /* eslint-enable @typescript-eslint/indent */
