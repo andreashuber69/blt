1c88ef0ec215df6d2aae9a8d67a69fcc76ecc792
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-07 14:06:31 +0200

    feat: make keys and return type readonly

--- src/lightning/pick.ts
+++ src/lightning/pick.ts
@@ -1,3 +1,3 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
-export const pick = <T extends object, K extends keyof T>(obj: T, keys: K[]) =>
-    Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]])) as Pick<T, K>;
+export const pick = <T extends object, K extends keyof T>(obj: T, keys: readonly K[]) =>
+    Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]])) as Readonly<Pick<T, K>>;
