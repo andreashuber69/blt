b7e7009f315d3ab38f23b593933da6821ed29bf5
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-07 19:27:54 +0200

    feat: have pick return deep readonly objects

--- src/lightning/pick.ts
+++ src/lightning/pick.ts
@@ -1,3 +1,5 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
+import type { DeepReadonly } from "../DeepReadonly.js";
+
 export const pick = <T extends object, K extends keyof T>(obj: T, keys: readonly K[]) =>
-    Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]])) as Readonly<Pick<T, K>>;
+    Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]])) as DeepReadonly<Pick<T, K>>;
