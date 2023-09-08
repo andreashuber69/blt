befe0fd2f56b3a2c0616df7bef458e0c2cac245e
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-08 11:43:04 +0200

    refactor: deduplicate tokens calculation

--- src/NodeStats.ts
+++ src/NodeStats.ts
@@ -27,12 +27,11 @@ export class NodeStats {
         }
 
         // eslint-disable-next-line @typescript-eslint/naming-convention
-        for (const { attempts, mtokens, fee_mtokens } of payments) {
-            const tokens = Number(mtokens) / 1000;
-            const fee = Number(fee_mtokens) / 1000;
+        for (const payment of payments) {
+            const { tokens, fee } = this.getTokens(payment);
 
             // eslint-disable-next-line @typescript-eslint/naming-convention
-            for (const { is_confirmed, confirmed_at, route: { hops } } of attempts) {
+            for (const { is_confirmed, confirmed_at, route: { hops } } of payment.attempts) {
                 if (is_confirmed && confirmed_at) {
                     this.add(this.getHistory(channelsImpl, hops.at(0)), confirmed_at, new Payment(tokens + fee));
                     this.add(this.getHistory(channelsImpl, hops.at(-1)), confirmed_at, new Payment(-tokens));
@@ -50,17 +49,17 @@ export class NodeStats {
     private static updateStats(
         channelStats: ReturnType<typeof getNewChannelStats> | undefined,
         isOut: boolean,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        { created_at, fee_mtokens, mtokens, outgoing_channel }: ForwardsElement,
+        forward: ForwardsElement,
     ) {
         if (channelStats) {
             const { [isOut ? "outgoingForwards" : "incomingForwards"]: forwardStats, history } = channelStats;
-            const tokens = Number(mtokens) / 1000;
-            const fee = Number(fee_mtokens) / 1000;
+            const { tokens, fee } = this.getTokens(forward);
             const real = isOut ? tokens : tokens + fee;
             forwardStats.maxTokens = Math.max(forwardStats.maxTokens, real);
             ++forwardStats.count;
             forwardStats.totalTokens += real;
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            const { created_at, outgoing_channel } = forward;
             const change = isOut ? new OutgoingForward(real, fee) : new IncomingForward(-real, outgoing_channel);
             this.add(history, created_at, change);
         }
@@ -74,6 +73,11 @@ export class NodeStats {
         if (history && !history.get(key)?.push(value)) {
             history.set(key, [value]);
         }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    private static getTokens({ fee_mtokens, mtokens }: { readonly fee_mtokens: string; readonly mtokens: string }) {
+        return { tokens: Number(mtokens) / 1000, fee: Number(fee_mtokens) / 1000 };
     }
 
     private constructor(public readonly channels: ReadonlyMap<string, ChannelStats>) {}
