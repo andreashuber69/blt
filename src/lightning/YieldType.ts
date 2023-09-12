fe9dd92b69224b6fcdecd01a68c5aff847d43e94
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-12 22:12:10 +0200

    feat: extend to normal Generator

--- src/lightning/YieldType.ts
+++ src/lightning/YieldType.ts
@@ -1,2 +1,3 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
-export type YieldType<T> = T extends AsyncGenerator<infer Element, void> ? Element : never;
+export type YieldType<T> =
+    T extends Generator<infer Element> ? Element : T extends AsyncGenerator<infer Element> ? Element : never;
