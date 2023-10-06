79681c40b046f75067218a1f34dd8e2db911703d
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-10-06 15:55:52 +0200

    refactor: simplify Date usage

--- src/info/getRange.ts
+++ src/info/getRange.ts
@@ -1,4 +1,4 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
-const getRangeAfter = (after: Date) => ({ after: after.toISOString(), before: new Date(Date.now()).toISOString() });
+const getRangeAfter = (after: Date) => ({ after: after.toISOString(), before: new Date().toISOString() });
 
 export const getRangeDays = (days: number) => getRangeAfter(new Date(Date.now() - (days * 24 * 60 * 60 * 1000)));
