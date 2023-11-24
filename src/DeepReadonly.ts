374e56a7b9951fce6f4f16c945c25a09e129ec28
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-11-24 11:45:36 +0100

    chore: update packages

--- src/DeepReadonly.ts
+++ src/DeepReadonly.ts
@@ -1,10 +1,10 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
 export type DeepReadonly<T> =
-    /* eslint-disable @typescript-eslint/indent */
+    /* eslint-disable @stylistic/indent */
     T extends Array<infer R> ? ReadonlyArray<DeepReadonly<R>> :
     T extends Map<infer K, infer V> ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>> :
     T extends Set<infer K> ? ReadonlySet<DeepReadonly<K>> :
     T extends (...args: never[]) => unknown ? T :
     T extends object ? { readonly [P in keyof T]: DeepReadonly<T[P]> } :
     T;
-    /* eslint-enable @typescript-eslint/indent */
+    /* eslint-enable @stylistic/indent */
