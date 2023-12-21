abad00180689ff5c91c37328c90d224d51dee810
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-12-21 14:30:18 +0100

    refactor: remove superfluous template string

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,5 +1,5 @@
 // https://github.com/andreashuber69/lightning-node-operator/develop/README.md
-const getCurrentTime = () => `${new Date().toLocaleTimeString(undefined, { hour12: false })}`;
+const getCurrentTime = () => new Date().toLocaleTimeString(undefined, { hour12: false });
 
 export const log = (message?: unknown) => {
     const separator = `${message}`.includes("\n") ? "\n" : " ";
